8c20a86cecdc86d0afdcda55de1da5299c607192
Semyon Evgrafov <nonseven@gmail.com>
2016-03-13 20:42:26 +0300

    Создан products_controller и создан файл products/index.html

--- spec/features/visit_index_spec.rb
+++ spec/features/visit_index_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "Visist landing page" do
   scenario do
     visit "/"
     expect(page).to have_content("Поиск мебели")
-    expect(page).to have_content("Войти")
+    expect(page).to have_content("Вход")
   end
 end
