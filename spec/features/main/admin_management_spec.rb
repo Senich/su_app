ba20fdb818d10a436519ea9695e129d76dfd5da8
Semyon Evgrafov <nonseven@gmail.com>
2016-04-07 23:11:39 +0300

    дабавлена вьюха для админки, добавлено логики в тест

--- spec/features/main/admin_management_spec.rb
+++ spec/features/main/admin_management_spec.rb
@@ -12,7 +12,15 @@ feature 'Управление пользователями' do
     link = "//a[contains(@href, '/admin')]"
     page.find(:xpath, link).click
     expect(page).to have_content('Управление пользователями')
-    expect(page).to have_content('Управление параметрами')
+    expect(page).to have_content('Управление элементами приложения')
+    click_link 'Управление пользователями'
+    expect(page).to have_content('Список участников')
+    click_link 'Добавить продавца'
+    fill_in 'Email', with: 'newuser@example.com'
+    fill_in 'Компания', with: 'ООО Вектор'
+    find('#password').fill_in 'Password', with: 'secret123'
+    find('#password_confirmation').fill_in 'Password confirmation', with: 'secret123'
+    click_button
   end
 
 end
