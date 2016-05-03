245bea482d23842a1553b4f8959bfd7d1cd66d6c
Semyon Evgrafov <nonseven@gmail.com>
2016-05-03 21:28:05 +0300

    Отрефакторена вьха для компаний, добавлено отображение списка компаний принадлежащих данному адресу

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -6,11 +6,14 @@ class Phone < ActiveRecord::Base
 
 
   def format_number
+    #вид 233-98-41
     if number.length == 7
       "#{number.to_s[0..2]}-#{number.to_s[3..4]}-#{number.to_s[5..6]}"
     elsif number.length == 10
+      #вид (921) 555-55-55
       "(#{number.to_s[0..2]}) #{number.to_s[3..5]}-#{number.to_s[6..7]}-#{number.to_s[8..9]}"
     elsif number.length == 11
+      #вид 8 (921) 555-55-55
       "#{number.to_s[0]} (#{number.to_s[1..3]}) #{number.to_s[4..6]}-#{number.to_s[7..8]}-#{number.to_s[9..10]}"
     else
       number
